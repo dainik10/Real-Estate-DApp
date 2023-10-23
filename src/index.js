15bb55f3c473a5dc34e06864a60e1d0cf8fbc711
Smit <smitmpatil123@gmail.com>
2023-10-23 09:47:02 +0530

    Wallet Connected

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { ContractProvider } from './context/ContractContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ContractProvider>
+        <App />
+    </ContractProvider>
 );
